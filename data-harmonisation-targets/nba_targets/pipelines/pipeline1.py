6bde409659c1fbb2c2d606333fb72c99c1566152
prakharmartand@prophecy.io <prakharmartand@prophecy.io>
2025-11-27 08:22:04 +0000

    Release tag : nba_targets/4 (CreatedAt: 2025-11-25 07:42:02.053)

--- data-harmonisation-targets/nba_targets/pipelines/pipeline1.py
+++ data-harmonisation-targets/nba_targets/pipelines/pipeline1.py
@@ -2,17 +2,6 @@ Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@g
 SensorSchedule = SensorSchedule(enabled = False)
 
 with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    cdm_player_brand_engagement = Task(
-        task_id = "cdm_player_brand_engagement", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {
-          "name": "cdm_player_brand_engagement", 
-          "sourceType": "Table", 
-          "sourceName": "qa_team_prakhartargets", 
-          "alias": ""
-        }
-    )
     cdm_player_financial_snapshot = Task(
         task_id = "cdm_player_financial_snapshot", 
         component = "Dataset", 
