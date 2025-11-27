6e1e9b639e8c2eb00827af63a44f8f1cb82368d7
prakharmartand@prophecy.io <prakharmartand@prophecy.io>
2025-11-27 08:22:04 +0000

    Release tag : nba_targets/3 (CreatedAt: 2025-11-25 07:22:27.561)

--- data-harmonisation-targets/nba_targets/pipelines/pipeline1.py
+++ data-harmonisation-targets/nba_targets/pipelines/pipeline1.py
@@ -24,12 +24,6 @@ with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
           "alias": ""
         }
     )
-    cdm_player_master = Task(
-        task_id = "cdm_player_master", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "cdm_player_master", "sourceType" : "Table", "sourceName" : "qa_team_prakhartargets", "alias" : ""}
-    )
     cdm_player_performance_aggregate = Task(
         task_id = "cdm_player_performance_aggregate", 
         component = "Dataset", 
@@ -40,4 +34,15 @@ with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
           "sourceName": "qa_team_prakhartargets", 
           "alias": ""
         }
+    )
+    new_cdm_brand_summary = Task(
+        task_id = "new_cdm_brand_summary", 
+        component = "Dataset", 
+        table = {"name" : "new_cdm_brand_summary", "sourceType" : "Table", "sourceName" : "qa_team_prakhartargets", "alias" : ""}
+    )
+    cdm_player_master = Task(
+        task_id = "cdm_player_master", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "cdm_player_master", "sourceType" : "Table", "sourceName" : "qa_team_prakhartargets", "alias" : ""}
     )
