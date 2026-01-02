4ac70d63359ab8e4f0da4a05bbd2c06c715550a3
prakharmartand@prophecy.io <prakharmartand@prophecy.io>
2026-01-02 09:45:00 +0000

    mcm

--- data-harmonisation-targets/nba_targets/pipelines/pipeline1.py
+++ data-harmonisation-targets/nba_targets/pipelines/pipeline1.py
@@ -1,7 +1,15 @@
-Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
-SensorSchedule = SensorSchedule(enabled = False)
+Schedules = [Schedule(
+               Name = "Schedule 1", 
+               emails = ["email@gmail.com"], 
+               emailOnStart = False, 
+               emailOnFailure = False, 
+               emailOnSuccess = False, 
+               enabled = False, 
+               cron = "* 0 2 * * * *", 
+               timezone = "GMT"
+             )]
 
-with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
+with DAG(schedules = Schedules):
     cdm_player_financial_snapshot = Task(
         task_id = "cdm_player_financial_snapshot", 
         component = "Dataset", 
